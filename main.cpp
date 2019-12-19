3b24e7d216e697b69656c77a2b69f9bb4e74d552
Marcelo <marcelosekundo@gmail.com>
2019-12-19 16:53:33 -0300

    Suporte a opções com getopt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <unistd.h>
+//#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	
-	std::cout << "Olá, mundo!\n";
-	
+int main(int argc, char *argv[]) {	
+	if(argc == 1) {
+		std::cout << "Olá, mundo!\n";
+	}
+	else {
+		int opt;
+		
+		opt = getopt(argc, argv, "bcdlnt");
+		
+		switch(opt) {
+			case 'b':
+				std::cout << "Good Morning, Good Morning!\n";
+				break;
+			case 'c':
+				std::cout << "Good Morning, Captain\n";
+				break;
+			case 'd':
+				std::cout << "Bom dia!\n";
+				break;
+			case 'l':
+				std::cout << "Bom lia!\n";
+				break;
+			case 'n':
+				std::cout << "Boa noite!\n";
+				break;
+			case 't':
+				std::cout << "Boa tarde!\n";
+				break;
+			default: /* '?' */
+				std::cout << "Hm.\n";
+				break;
+		}
+	}
 	return 0;
 }
